93784a0cfbff90b98eb6f2974eb9c172c373b289
Mo Shahryar <shaharyar.ms@outlook.com>
2023-04-05 00:59:02 +0500

    Map view dummy locations and levels data with images

--- src/components/AssetMapView.js
+++ src/components/AssetMapView.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Drawer, Paper } from "@mui/material";
+import { Box, Button, Grid, Drawer, Paper, Typography } from "@mui/material";
 import { ExpandMore, ChevronRight } from "@mui/icons-material";
 import { TreeView, TreeItem } from "@mui/lab";
 import { useState } from "react";
@@ -6,17 +6,31 @@ import { useState } from "react";
 import MapPin from "./MapPin";
 
 import mezzMap from "../assets/Mezz-Floor.png";
+import roofMap from "../assets/Roof-Deck.png";
+import lowerMap from "../assets/Lower-Deck.png";
 
 const AssetMapView = ({ data }) => {
   const [expanded, setExpanded] = useState([]);
   const [selected, setSelected] = useState([]);
 
+  const [selectedMap, setSelectedMap] = useState("");
+
   const handleToggle = (event, nodeIds) => {
     setExpanded(nodeIds);
   };
 
-  const handleSelect = (event, nodeIds) => {
-    setSelected(nodeIds);
+  const handleSelect = (event, nodeId) => {
+    if (nodeId.includes("location")) {
+    setSelected(nodeId);
+    console.log("location");
+    }
+
+    if (nodeId.includes("level")) {
+      if (nodeId.includes("roofDeck")) setSelectedMap(roofMap);
+      if (nodeId.includes("lowerDeck")) setSelectedMap(lowerMap);
+      if (nodeId.includes("mezzFloor")) setSelectedMap(mezzMap);
+      setSelected(nodeId);
+    }
   };
 
   const handleExpandClick = () => {
@@ -25,13 +39,13 @@ const AssetMapView = ({ data }) => {
     );
   };
 
-  const handleSelectClick = () => {
-    setSelected((oldSelected) =>
-      oldSelected.length === 0
-        ? ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        : []
-    );
-  };
+  // const handleSelectClick = () => {
+  //   setSelected((oldSelected) =>
+  //     oldSelected.length === 0
+  //       ? ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  //       : []
+  //   );
+  // };
 
   return (
     <Grid container spacing={2}>
@@ -53,19 +67,26 @@ const AssetMapView = ({ data }) => {
             selected={selected}
             onNodeToggle={handleToggle}
             onNodeSelect={handleSelect}
-            multiSelect
           >
-            <TreeItem nodeId="1" label="Applications">
-              <TreeItem nodeId="2" label="Calendar" />
-              <TreeItem nodeId="3" label="Chrome" />
-              <TreeItem nodeId="4" label="Webstorm" />
+            <TreeItem nodeId="location-1" label="Location 1">
+              <TreeItem nodeId="level-roofDeck" label="Roof Deck">
+                <TreeItem nodeId="eq-1" label="Equipment 1" />
+                <TreeItem nodeId="eq-2" label="Equipment 2" />
+                <TreeItem nodeId="eq-3" label="Equipment 3" />
+                <TreeItem nodeId="eq-4" label="Equipment 4" />
+              </TreeItem>
+              <TreeItem nodeId="level-lowerDeck" label="Lower Deck">
+                <TreeItem nodeId="eq-5" label="Equipment 5" />
+                <TreeItem nodeId="eq-6" label="Equipment 6" />
+                <TreeItem nodeId="eq-7" label="Equipment 7" />
+                <TreeItem nodeId="eq-8" label="Equipment 8" />
+              </TreeItem>
             </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-              <TreeItem nodeId="6" label="MUI">
-                <TreeItem nodeId="7" label="src">
-                  <TreeItem nodeId="8" label="index.js" />
-                  <TreeItem nodeId="9" label="tree-view.js" />
-                </TreeItem>
+            <TreeItem nodeId="location-2" label="Location 2">
+              <TreeItem nodeId="level-mezzFloor" label="Mezz Floor">
+                <TreeItem nodeId="eq-9" label="Equipment 9"></TreeItem>
+                <TreeItem nodeId="eq-10" label="Equipment 10"></TreeItem>
+                <TreeItem nodeId="eq-11" label="Equipment 11"></TreeItem>
               </TreeItem>
             </TreeItem>
           </TreeView>
@@ -73,17 +94,32 @@ const AssetMapView = ({ data }) => {
       </Grid>
       <Grid item xs={12} md={9}>
         <Paper elevation={2}>
-          <div
-            style={{
+          <Box
+            sx={{
               width: "100%",
-              height: 400,
-              backgroundImage: `url(${mezzMap})`,
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
+              minHeight: 120,
               position: "relative",
             }}
           >
+            {selectedMap ? (
+              <img
+                style={{ width: "100%" }}
+                src={selectedMap}
+                alt="Level Map"
+              />
+            ) : (
+              <Box
+                sx={{
+                  p: 4,
+                  display: "flex",
+                  height: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography>Select a level to view map</Typography>
+              </Box>
+            )}
             {data.map((dataPin) => (
               <MapPin
                 x={Math.floor(Math.random() * 100)}
@@ -91,7 +127,7 @@ const AssetMapView = ({ data }) => {
                 data={dataPin}
               />
             ))}
-          </div>
+          </Box>
         </Paper>
       </Grid>
     </Grid>
