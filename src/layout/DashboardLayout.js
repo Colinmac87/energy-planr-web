0d5f61048e1b602073619e972a68c5273c6282e7
Mo Shahryar <shaharyar.ms@outlook.com>
2023-05-26 12:55:59 +0500

    User name display in nav bar

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -3,27 +3,29 @@ import {
   Box,
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
   Container,
   Link,
   MenuItem,
   Menu,
+  Button,
 } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signOut } from "../services/auth.service";
+import { setUser } from "../features/account.slice";
 
 const DashboardLayout = ({ children }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = useState(null);
 
-  const account = useSelector((state) => state.account);
+  const { loginAttempted, user } = useSelector((state) => state.account);
 
   useEffect(() => {
-    if (account.loginAttempted && !account.user) navigate("/signin");
-  }, [account.loginAttempted]);
+    if (loginAttempted && !user) navigate("/signin");
+  }, [loginAttempted]);
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -41,11 +43,12 @@ const DashboardLayout = ({ children }) => {
   const onClickSignout = () => {
     handleClose();
     signOut().then(() => {
+      dispatch(setUser(null));
       navigate("/signin");
     });
   };
 
-  if (!account.loginAttempted) return null;
+  if (!loginAttempted || !user) return null;
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -76,16 +79,18 @@ const DashboardLayout = ({ children }) => {
             </Link>
           </Box>
           <div>
-            <IconButton
+            <Button
               size="large"
+              endIcon={<AccountCircle />}
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleMenu}
               color="inherit"
+              sx={{ textTransform: "capitalize" }}
             >
-              <AccountCircle />
-            </IconButton>
+              {user.fullName}
+            </Button>
 
             <Menu
               id="menu-appbar"
