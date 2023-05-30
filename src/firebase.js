3547bb377bc13a913dc7757e370527880f2b0932
Mo Shahryar <shaharyar.ms@outlook.com>
2023-05-30 20:27:48 +0500

    Firebase config comments remove

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore, initializeFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -33,7 +29,6 @@ export const initFirebase = () => {
     experimentalForceLongPolling: true,
   });
 
-  // db = getFirestore(app);
   storage = getStorage(app);
 };
 
